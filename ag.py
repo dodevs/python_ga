c3e14b03e0aa219bded21e797ef3ffb16e55aaa0
dodevs <douglass.sousa@outlook.com.br>
2019-05-22 23:48:58 -0300

    Primeiro commit

---
+++ ag.py
@@ -0,0 +1,89 @@
+'''
+Objetivo: Minimizar f(x)=x²−3x+4
+Critérios
+    Dominio: x∈[−10,+10];
+    Criar uma população inicial com 4 indivíduos;
+    Usar seleção por torneio (n = 2);
+    Aplicar Crossover com taxa de 60% (Crossover de 1 ponto uniforme);
+    Aplicar Mutação com taxa de 1%;
+    Usar 5 gerações e 10 gerações;
+    Normalizacao:
+        x = min + (max-min) * (b10/2l - 10), onde:
+            b10 = binario(cromossomo) na base 10
+            2l = 2 elevado ao numero de bits
+
+1. Gerar a população inicial.
+2. Avaliar cada indivíduo da população.
+3. Enquanto critério de parada não for satisfeito faça
+    3.1  Selecionar os indivíduos mais aptos.
+    3.2  Criar novos indivíduos aplicando os operadores 
+        crossover e mutação.
+    3.3  Armazenar os novos indivíduos em umanova população.
+    3.4 Avaliar cada indivíduo da nova população.
+'''
+
+class Populacao:
+    def __init__(self, qtdIndividuos, dominio, fitnessFunc, precisao):
+        self.qtdIndividuos = qtdIndividuos
+        self.dominio = dominio
+        self.precisao = precisao
+        self.fitnessFunc = fitnessFunc
+        self.individuos = self._gerarIndividuos()
+        self.geracaoAtual = 0
+
+    def _gerarIndividuos(self):
+        from random import randint
+        return [
+            individuo(
+                ''.join(map(str, [randint(0,1) for _ in range(self.precisao)])) #Binario
+            ) for _ in range(self.qtdIndividuos) 
+        ]
+
+    def _normalizacao(self, cromossomo):
+        valor = int(cromossomo, 2)
+        valorNormalizado = self.dominio[0] + (self.dominio[1] - self.dominio[0]) * (valor / (2 ** self.precisao - 1))
+        return valorNormalizado
+
+    def _selecao(self):
+        pass
+
+    def avaliacao(self):
+        for individuo in self.individuos:
+            valorNormalizado = self._normalizacao(individuo.cromossomo)
+            individuo.fitness = self.fitnessFunc(valorNormalizado)
+
+    def novaGeracao(self):
+        selecionados = None;
+
+
+class individuo:
+    def __init__(self, cromossomo: str):
+        self.cromossomo = cromossomo
+        self.fitness = None
+
+    def __eq__(self, value):
+        return self.fitness == value.fitness
+
+    def __le__(self, value):
+        return self.fitness <= value.fitness
+
+    def __lt__(self, value):
+        return self.fitness < value.fitness
+
+
+
+def fitness(x):
+    return x ** 2 - 3 * x + 4
+
+def mutacao(x):
+    '{"""}'.format()
+
+def main():
+    populacao = Populacao(4, [-10,10], fitness, 10)
+    populacao.avaliacao()
+    print([individuo.fitness for individuo in populacao.individuos])
+    while populacao.geracaoAtual <= 5:
+        populacao.novaGeracao()
+
+if __name__ == "__main__":
+    main()
