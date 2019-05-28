7e19d2bd089e351582075f2640338eda56ad1f17
dodevs <douglass.sousa@outlook.com.br>
2019-05-28 12:42:28 -0300

    Modularizado o programa e melhorado funcao de plot com base em execucoes

--- ag.py
+++ ag.py
@@ -1,168 +1,30 @@
-class Populacao:
-    def __init__(self, qtdIndividuos, dominio, fitnessFunc, precisao, taxaCrossover, taxaMutacao):
-        self.qtdIndividuos = qtdIndividuos
-        self.dominio = dominio
-        self.precisao = precisao
-        self.fitnessFunc = fitnessFunc
-        self.taxaCrossover = taxaCrossover / 100
-        self.taxaMutacao = taxaMutacao / 100
-        self.individuos = self._gerarIndividuos()
-        self.geracaoAtual = 0
-
-    def _gerarIndividuos(self):
-        from random import randint
-        return [
-            Individuo(
-                [randint(0,1) for _ in range(self.precisao)]
-            ) for _ in range(self.qtdIndividuos)
-        ]
-
-    def _crossover(self, selecionados):
-        from random import randint, uniform
-
-        lenS = len(selecionados)
-        filhos = []
-
-        i = 0
-        while i < lenS:
-            pai1 = selecionados[i]
-            if i+1 == lenS:
-                pai2 = selecionados[lenS * -1]
-            else:
-                pai2 = selecionados[i+1]
-
-            if uniform(0,1) <= self.taxaCrossover:
-
-                pontoDeCorte = randint(0, self.precisao)
-                novoCromossomo1 = pai1.cromossomo[:pontoDeCorte] + pai2.cromossomo[pontoDeCorte:]
-                novoCromossomo2 = pai2.cromossomo[:pontoDeCorte] + pai1.cromossomo[pontoDeCorte:]
-                filho1 = Individuo(novoCromossomo1)
-                filho2 = Individuo(novoCromossomo2)
-                filhos.append(filho1)
-                filhos.append(filho2)
-
-            else:
-                filhos.append(pai1)
-                filhos.append(pai2)
-            i+= 2
-
-        return filhos
-
-
-    def _selecao(self):
-        from random import choice
-
-        selecionados = []
-
-        while len(selecionados) < self.qtdIndividuos:
-            desafiante1 = choice(self.individuos)
-            desafiante2 = choice(self.individuos)
-            selecionados.append(min(desafiante1, desafiante2))
-
-        return selecionados
-
-
-    def _mutacao(self,selecionados):
-        from random import uniform
-
-        for individuo in selecionados:
-            for i in range(self.precisao):
-                if(uniform(0,1) <= self.taxaMutacao):
-                    individuo.cromossomo[i] = int(not individuo.cromossomo[i])
-
-    def _normalizacao(self, cromossomo):
-        valorNormalizado = (self.dominio[0] + (self.dominio[1] - self.dominio[0])) * cromossomo / (2 ** self.precisao - 1)
-        return valorNormalizado
-
-    def avaliacao(self):
-        for individuo in self.individuos:
-            valorNormalizado = self._normalizacao(individuo.cromossomo_int())
-            individuo.fitness = self.fitnessFunc(valorNormalizado)
-
-    def novaGeracao(self):
-        selecionados = self._selecao()
-        novaGeracao = self._crossover(selecionados)
-        self._mutacao(novaGeracao)
-        self.individuos = novaGeracao
-        self.geracaoAtual += 1
-
-
-class Individuo:
-    def __init__(self, cromossomo):
-        self.cromossomo = cromossomo
-        self.fitness = None
-
-    def __eq__(self, value):
-        return self.fitness == value.fitness
-
-    def __le__(self, value):
-        return self.fitness <= value.fitness
-
-    def __lt__(self, value):
-        return self.fitness < value.fitness
-
-    def cromossomo_str(self):
-        return ''.join(map(str, self.cromossomo))
-
-    def cromossomo_int(self):
-        return int(self.cromossomo_str(), 2)
-
-def mediaIndividuos(individuos, qtdIndividuos):
-    soma_fitness = 0
-    for individuo in individuos:
-        soma_fitness += individuo.fitness
-
-    return soma_fitness/qtdIndividuos
-
-def salvaGeracao(individuos, geracao):
-    file = open("resultados/geracao_{0}.txt".format(geracao), "a+")
-    for individuo in individuos:
-        file.write("{0}\n".format(individuo.fitness))
-
-    file.close()
+from models import Populacao
+from utils import salvaResultado, mediaIndividuos, plotaResultados
 
 def fitness(x):
     return x ** 2 - 3 * x + 4
 
 def main():
     import matplotlib.pyplot as plt
+    from time import sleep
     from functools import reduce
 
-    plot_y_media = []
-    plot_y_melhor = []
-    plot_x = [] # iteracao
+    numero_execucoes = 10
+    numero_geracoes = 10
 
-    # Parametros: qtdIndividuos, dominio, funcao fitness, precisao, taxa crossover, taxa mutacao
-    populacao = Populacao(4, [-10,10], fitness, 10, 60, 1)
-    populacao.avaliacao()
-
-    while populacao.geracaoAtual < 10:
-        # PyPlot data
-        plot_x.append(populacao.geracaoAtual)
-        plot_y_media.append(mediaIndividuos(populacao.individuos, populacao.qtdIndividuos))
-        plot_y_melhor.append(min(populacao.individuos).fitness)
-
-        #salvaGeracao(populacao.individuos, populacao.geracaoAtual)
-
-        # Ciclo do algoritmo
-        populacao.novaGeracao()
+    for execucao in range(numero_execucoes):
+        # Parametros: qtdIndividuos, dominio, funcao fitness, precisao, taxa crossover, taxa mutacao
+        populacao = Populacao(4, [-10,10], fitness, 10, 60, 1)
         populacao.avaliacao()
 
+        while populacao.geracaoAtual < numero_geracoes:
+            salvaResultado(execucao, populacao.geracaoAtual, populacao.individuos)
 
-    # PyPloat Config
-    plt.title('Algoritmo Genético')
-    plt.xlabel('Gerações')
-    plt.ylabel('Fitness')
-    media_line, = plt.plot(plot_x, plot_y_media, color='y')
-    best_line, = plt.plot(plot_x, plot_y_melhor, color='g')
-    plt.legend([media_line, best_line], ['Média; Ultima geração: {0}'.format(plot_y_media[-1]), 'Melhor; Ultima geração: {0}'.format(plot_y_melhor[-1])])
+            # Ciclo do algoritmo
+            populacao.novaGeracao()
+            populacao.avaliacao()
 
-    #plt.text(plot_x[-1], plot_y_melhor[-1], 'Melhor fitness: {}'.format(plot_y_melhor[-1]), horizontalalignment='right')
-    #plt.text(plot_x[-1], plot_y_media[-1], 'Media fitness: {}'.format(plot_y_media[-1]), horizontalalignment='left')
-
-    #plt.ylim(-10, 10)
-    plt.xticks(plot_x)
-    plt.show()
+    plotaResultados(numero_execucoes, numero_geracoes, populacao.qtdIndividuos)
 
 if __name__ == "__main__":
     main()
