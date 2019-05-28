db2000d375c555fd3bfd7393dea049b74d9f1799
dodevs <douglass.sousa@outlook.com.br>
2019-05-28 12:32:48 -0300

    Modularizado o programa e melhorado funcao de plot com base em execucoes

---
+++ models.py
@@ -0,0 +1,116 @@
+class Populacao:
+    def __init__(self, qtdIndividuos, dominio, fitnessFunc, precisao, taxaCrossover, taxaMutacao):
+        self.qtdIndividuos = qtdIndividuos
+        self.dominio = dominio
+        self.precisao = precisao
+        self.fitnessFunc = fitnessFunc
+        self.taxaCrossover = taxaCrossover / 100
+        self.taxaMutacao = taxaMutacao / 100
+        self.individuos = self._gerarIndividuos()
+        self.geracaoAtual = 0
+
+    def _gerarIndividuos(self):
+        from random import randint
+        return [
+            Individuo(
+                [randint(0,1) for _ in range(self.precisao)]
+            ) for _ in range(self.qtdIndividuos)
+        ]
+
+    def _crossover(self, selecionados):
+        from random import randint, uniform
+
+        lenS = len(selecionados)
+        filhos = []
+
+        i = 0
+        while i < lenS:
+            pai1 = selecionados[i]
+            if i+1 == lenS:
+                pai2 = selecionados[lenS * -1]
+            else:
+                pai2 = selecionados[i+1]
+
+            if uniform(0,1) <= self.taxaCrossover:
+
+                pontoDeCorte = randint(0, self.precisao)
+                novoCromossomo1 = pai1.cromossomo[:pontoDeCorte] + pai2.cromossomo[pontoDeCorte:]
+                novoCromossomo2 = pai2.cromossomo[:pontoDeCorte] + pai1.cromossomo[pontoDeCorte:]
+                filho1 = Individuo(novoCromossomo1)
+                filho2 = Individuo(novoCromossomo2)
+                filhos.append(filho1)
+                filhos.append(filho2)
+
+            else:
+                filhos.append(pai1)
+                filhos.append(pai2)
+
+            i+= 2
+
+        #self._elitismo(selecionados, filhos)
+        return filhos
+
+
+    def _selecao(self):
+        from random import choice
+
+        selecionados = []
+
+        while len(selecionados) < self.qtdIndividuos:
+            desafiante1 = choice(self.individuos)
+            desafiante2 = choice(self.individuos)
+            selecionados.append(min(desafiante1, desafiante2))
+
+        return selecionados
+
+
+    def _mutacao(self,selecionados):
+        from random import uniform
+
+        for individuo in selecionados:
+            for i in range(self.precisao):
+                if(uniform(0,1) <= self.taxaMutacao):
+                    individuo.cromossomo[i] = int(not individuo.cromossomo[i])
+
+    def _elitismo(self, selecionados, filhos):
+        if(min(selecionados) < min(filhos)):
+            for i in range(len(filhos)):
+                if(filhos[i] == max(filhos)):
+                    filhos[i] = min(selecionados)
+
+    def _normalizacao(self, cromossomo):
+        valorNormalizado = (self.dominio[0] + (self.dominio[1] - self.dominio[0])) * cromossomo / (2 ** self.precisao - 1)
+        return valorNormalizado
+
+    def avaliacao(self):
+        for individuo in self.individuos:
+            valorNormalizado = self._normalizacao(individuo.cromossomo_int())
+            individuo.fitness = self.fitnessFunc(valorNormalizado)
+
+    def novaGeracao(self):
+        selecionados = self._selecao()
+        novaGeracao = self._crossover(selecionados)
+        self._mutacao(novaGeracao)
+        self.individuos = novaGeracao
+        self.geracaoAtual += 1
+
+
+class Individuo:
+    def __init__(self, cromossomo):
+        self.cromossomo = cromossomo
+        self.fitness = None
+
+    def __eq__(self, value):
+        return self.fitness == value.fitness
+
+    def __le__(self, value):
+        return self.fitness <= value.fitness
+
+    def __lt__(self, value):
+        return self.fitness < value.fitness
+
+    def cromossomo_str(self):
+        return ''.join(map(str, self.cromossomo))
+
+    def cromossomo_int(self):
+        return int(self.cromossomo_str(), 2)
---
+++ utils.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+
+def plotaResultados(numeroExecucoes, qtdGeracoes, qtdIndividuos):
+    plot_y_media = []
+    plot_y_melhor = []
+    plot_x = [] # iteracao
+
+    for execucao in range(numeroExecucoes):
+        media_execucao = 0
+        menor_execucao = float('inf')
+
+        for geracao in range(qtdGeracoes):
+            soma_geracao = 0
+            menor_geracao = float('inf')
+            file = open("resultados/{0}_{1}_{2}.txt".format(execucao, geracao, qtdIndividuos), 'r')
+            fitness = file.readline()
+            while fitness != "":
+                fitness_float = float(fitness)
+
+                soma_geracao += fitness_float
+                if fitness_float < menor_geracao:
+                    menor_geracao = fitness_float
+
+                fitness = file.readline()
+            
+            media_execucao += soma_geracao/qtdGeracoes
+            if menor_geracao < menor_execucao:
+                menor_execucao = menor_geracao
+
+        plot_x.append(execucao)
+        plot_y_media.append(media_execucao)
+        plot_y_melhor.append(menor_execucao)
+
+    # PyPloat Config
+    plt.title('Algoritmo Genético')
+    plt.xlabel('Execucão')
+    plt.ylabel('Fitness')
+    media_line, = plt.plot(plot_x, plot_y_media, color='y')
+    best_line, = plt.plot(plot_x, plot_y_melhor, color='g')
+    plt.legend([media_line, best_line], ['Média; Ultima execução: {0}'.format(plot_y_media[-1]), 'Melhor; Ultima execução: {0}'.format(plot_y_melhor[-1])])
+
+    #plt.ylim(-10, 10)
+    plt.xticks(plot_x)
+    plt.show()
+
+def mediaIndividuos(individuos, qtdIndividuos):
+    soma_fitness = 0
+    for individuo in individuos:
+        soma_fitness += individuo.fitness
+
+    return soma_fitness/qtdIndividuos
+
+def salvaResultado(execucao, geracao, individuos):
+    file = open("resultados/{0}_{1}_{2}.txt".format(execucao, geracao, len(individuos)), "w")
+    for individuo in individuos:
+        file.write("{0}\n".format(individuo.fitness))
+
+    file.close()
