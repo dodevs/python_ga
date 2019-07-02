4635e2af4818199fbeca4e8fb353e256c832875b
dodevs <douglass.sousa@outlook.com.br>
2019-07-02 12:47:23 -0300

    Alterada chamada para testes

--- ag.py
+++ ag.py
@@ -5,22 +5,22 @@ def fitness(x):
     return x ** 2 - 3 * x + 4
 
 def main():
-    numero_execucoes = 10
-    numero_geracoes = 5
+    numero_execucoes = 1
+    numero_geracoes = 50
 
     for execucao in range(numero_execucoes):
         # Parametros: qtdIndividuos, dominio, funcao fitness, precisao, taxa crossover, taxa mutacao
-        populacao = Populacao(4, [-10,10], fitness, 10, 60, 1)
+        populacao = Populacao(4, [-10,10], fitness, 100000, 60, 10)
         populacao.avaliacao()
 
         while populacao.geracaoAtual < numero_geracoes:
-            salvaResultado(execucao, populacao.geracaoAtual, populacao.individuos)
+            #salvaResultado(execucao, populacao.geracaoAtual, populacao.individuos)
 
             # Ciclo do algoritmo
             populacao.novaGeracao()
             populacao.avaliacao()
 
-    plotaResultados(numero_execucoes, numero_geracoes, populacao.qtdIndividuos)
+    #plotaResultados(numero_execucoes, numero_geracoes, populacao.qtdIndividuos)
 
 if __name__ == "__main__":
     main()
