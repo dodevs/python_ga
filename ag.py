1a02008859a5658bcfecd3ca4f9e4dddc9ba3e6e
dodevs <douglass.sousa@outlook.com.br>
2019-07-02 12:43:17 -0300

    Alterado chamada da aplicação para testes

--- ag.py
+++ ag.py
@@ -1,12 +1,16 @@
 from models import Populacao
 from utils import salvaResultado, mediaIndividuos, plotaResultados
+from functools import reduce
+from operator import add
 
 def fitness(x):
     return x ** 2 - 3 * x + 4
 
 def main():
-    numero_execucoes = 10
+    numero_execucoes = 1
     numero_geracoes = 5
+
+    medias_geracao = []
 
     for execucao in range(numero_execucoes):
         # Parametros: qtdIndividuos, dominio, funcao fitness, precisao, taxa crossover, taxa mutacao
@@ -22,13 +26,17 @@ def main():
         populacao.avaliacao()
 
         while populacao.geracaoAtual < numero_geracoes:
-            salvaResultado(execucao, populacao.geracaoAtual, populacao.individuos)
-
+            #salvaResultado(execucao, populacao.geracaoAtual, populacao.individuos)
+            medias_geracao.append(
+                #mediaIndividuos(populacao.individuos, populacao.qtdIndividuos)
+                reduce(add, populacao.individuos) / populacao.qtdIndividuos
+            )
             # Ciclo do algoritmo
             populacao.novaGeracao()
             populacao.avaliacao()
 
-    plotaResultados(numero_execucoes, numero_geracoes, populacao.qtdIndividuos)
+    #plotaResultados(numero_execucoes, numero_geracoes, populacao.qtdIndividuos)
+    print(medias_geracao)
 
 if __name__ == "__main__":
     main()
